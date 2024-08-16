40c666f6ea62420222a5415232b052bfbf541f4c
shehab910 <shehabihab@hotmail.com>
2024-08-16 22:32:12 +0300

    cleanup

--- frontend/orval/custom-instance.ts
+++ frontend/orval/custom-instance.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 export const AXIOS_INSTANCE = Axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
 }); // use your own URL here or environment variable
 
 // add a second `options` argument here if you want to pass extra options to each generated query
